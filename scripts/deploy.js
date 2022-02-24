bfa244f334ee27ecf94ea68e3abd8b6dbf530bc8
benbaessler <mail@benbaessler.com>
2022-02-24 17:11:30 +0000

    Added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Nevam = await hre.ethers.getContractFactory("Nevam");
+  const contract = await Nevam.deploy();
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+
+  await contract.mintBatch([1, 2, 3], [210, 150, 40])
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
