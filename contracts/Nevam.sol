d197ebd8417988c6eab5c4cf256a91c9a8e0723b
benbaessler <mail@benbaessler.com>
2022-02-24 18:32:32 +0000

    Refactored mint requirements

--- contracts/Nevam.sol
+++ contracts/Nevam.sol
@@ -6,29 +6,28 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Nevam is ERC1155, Ownable {
 
+  enum SaleStatus { CLOSED, PRESALE, PUBLIC }
+
   uint256 public constant TIER_1_SUPPLY = 1410;
   uint256 public constant TIER_2_SUPPLY = 650;
   uint256 public constant TIER_3_SUPPLY = 140;
 
-  mapping (address => bool) public mintedTier1;
-  mapping (address => bool) public mintedTier2;
-  mapping (address => bool) public mintedTier3;
+  mapping(uint256 => uint256) public amountLeft;
+  mapping(uint256 => mapping(address => bool)) public mintedTier;
 
-  constructor() ERC1155("ipfs://QmeEW8VV7gzTd64dcgYmw7EL2QqrRszNzwopGV9VVt8XC9/{id}.json") {}
+  constructor() ERC1155("ipfs://QmeEW8VV7gzTd64dcgYmw7EL2QqrRszNzwopGV9VVt8XC9/{id}.json") {
+    amountLeft[1] = TIER_1_SUPPLY;
+    amountLeft[2] = TIER_2_SUPPLY;
+    amountLeft[3] = TIER_3_SUPPLY;
+  }
 
   function mint(uint256 _id) public {
     require(_id < 4, "Invalid token ID");
+    require(amountLeft[_id] > 0, "All tokens are already minted");
+    require(mintedTier[_id][msg.sender], "You already minted this token");
 
-    if (_id == 1) {
-      require(mintedTier1[msg.sender] == false, "You already minted this token");
-      mintedTier1[msg.sender] = true;
-    } else if (_id == 2) {
-      require(mintedTier2[msg.sender] == false, "You already minted this token");
-      mintedTier2[msg.sender] = true;
-    } else {
-      require(mintedTier3[msg.sender] == false, "You already minted this token");
-      mintedTier3[msg.sender] = true;
-    }
+    mintedTier[_id][msg.sender] = true;
+    amountLeft[_id] -= 1;
 
     _mint(msg.sender, _id, 1, "");
   }
