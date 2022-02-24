03c9897eb442a2a5739b2b7db6dcc9a0d1607489
benbaessler <mail@benbaessler.com>
2022-02-24 16:23:02 +0000

    Added test metadata + mint functions

--- contracts/Nevam.sol
+++ contracts/Nevam.sol
@@ -6,10 +6,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Nevam is ERC1155, Ownable {
 
+  uint256 public constant TIER_1 = 1;
+  uint256 public constant TIER_2 = 2;
+  uint256 public constant TIER_3 = 3;
+
   uint256 public constant TIER_1_SUPPLY = 1410;
   uint256 public constant TIER_2_SUPPLY = 650;
   uint256 public constant TIER_3_SUPPLY = 140;
 
-  constructor() ERC1155("") {}
+  constructor() ERC1155("ipfs://QmeEW8VV7gzTd64dcgYmw7EL2QqrRszNzwopGV9VVt8XC9/{id}.json") {}
+
+  function mint(uint256 _id) public {
+    _mint(msg.sender, _id, 1, "");
+  }
+
+  function mintBatch(uint256[] memory _ids, uint256[] memory _amounts) public onlyOwner {
+    _mintBatch(msg.sender, _ids, _amounts, "");
+  }
 
 }
