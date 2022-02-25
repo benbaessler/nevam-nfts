509dc5c9e0959635c9bde47763a852cd81d22fcc
benbaessler <mail@benbaessler.com>
2022-02-25 17:17:46 +0000

    Added sale status + batch minting tests

--- test/test.js
+++ test/test.js
@@ -5,12 +5,11 @@ describe('Token contract', () => {
 
   let contract;
   let owner;
-  let user1;
-  let user2;
+  let user;
 
   beforeEach(async () => {
     const Nevam = await ethers.getContractFactory('Nevam');
-    [owner, user1, user2] = await ethers.getSigners();
+    [owner, user] = await ethers.getSigners();
 
     contract = await Nevam.deploy()
   })
@@ -28,36 +27,96 @@ describe('Token contract', () => {
     })
   })
 
-  describe('Minting', () => {
-    it('Should not allow minting if sale is closed', async () => {
+  describe('Sale status', () => {
+    it('Should not be able to mint if sale is closed', async () => {
       await expect(contract.mint(1)).to.be.revertedWith('Sale is not active')
     })
 
-    it('Should not mint token IDs over 3', async () => {
+    it('Should only be able for whitelisted accounts to mint during presale', async () => {
+      await contract.setSaleStatus(1)
+      await contract.setWhitelist([user.address])
+
+      await expect(contract.mint(1)).to.be.revertedWith('You are not whitelisted')
+
+      await contract.connect(user).mint(1)
+
+      const response = await contract.balanceOf(user.address, 1)
+      expect(response).to.equal(1)
+    })
+
+    it('Should be able for anyone to mint during public sale', async () => {
       await contract.setSaleStatus(2)
+      await contract.setWhitelist([user.address])
+
+      await contract.mint(1)
+      await contract.connect(user).mint(1)
+
+      let response = await contract.balanceOf(owner.address, 1)
+      expect(response).to.equal(1)
+
+      response = await contract.balanceOf(user.address, 1)
+      expect(response).to.equal(1)
+    })
+  })
+
+  describe('Minting', () => {
+    beforeEach(async () => { await contract.setSaleStatus(2) })
+
+    it('Should not mint token IDs over 3', async () => {
       await expect(contract.mint(4)).to.be.revertedWith('Invalid token ID')
     })
 
     it('Should not mint if there is no supply', async () => {
-      await contract.setSaleStatus(2)
       await contract.mintPrivate([1, 2, 3], [1410, 650, 140])
 
       await expect(contract.mint(1)).to.be.revertedWith('All tokens with this ID were already minted')
     })
 
     it('Should not be able to mint a token twice', async () => {
-      await contract.setSaleStatus(2)
       await contract.mint(1)
 
       await expect(contract.mint(1)).to.be.revertedWith('You already minted this token')
     })
 
     it('Should update the supply', async () => {
-      await contract.setSaleStatus(2)
       await contract.mint(1)
 
       const response = await contract.amountLeft(1)
       expect(response).to.equal(1409)
+    })
+
+    it('Should update the users token balance', async () => {
+      await contract.connect(user).mint(1)
+
+      const response = await contract.balanceOf(user.address, 1)
+      expect(response).to.equal(1)
+    })
+  })
+
+  describe('Batch minting', () => {
+    beforeEach(async () => { await contract.setSaleStatus(2) })
+
+    it('Should not be able to mint more than 3 tokens', async () => { 
+      await expect(contract.mintBatch([1, 2, 3, 4])).to.be.revertedWith('You can only mint a maximum of 3 tokens')
+    })
+
+    it('Should revert if the user has already minted one of the tokens', async () => {
+      await contract.mintBatch([1, 2])
+
+      await expect(contract.mintBatch([2, 3])).to.be.revertedWith('You already minted this token')
+    })
+
+    it('Should update the supply of all minted tokens', async () => {
+      await contract.mintBatch([1, 2, 3])
+
+      response = await contract.amountLeft(1)
+      expect(response).to.equal(1409)
+
+      response = await contract.amountLeft(2)
+      expect(response).to.equal(649)
+
+      response = await contract.amountLeft(3)
+      expect(response).to.equal(139)
     })
   })
 
@@ -87,40 +146,8 @@ describe('Token contract', () => {
     })
 
     it('Should not be able for user to mint team-allocated tokens', async () => {
-      await expect(contract.connect(user1).mintPrivate([1, 2, 3], [10, 10, 10]))
+      await expect(contract.connect(user).mintPrivate([1, 2, 3], [10, 10, 10]))
         .to.be.revertedWith('Ownable: caller is not the owner')
     })
-  })
-
-  describe('Presale minting', async () => {
-    it('Should be able to whitelist addresses', async () => {
-      let response = await contract.whitelisted(user1.address)
-      expect(response).to.equal(false)
-
-      await contract.setWhitelist([user1.address])
-
-      response = await contract.whitelisted(user1.address)
-      expect(response).to.equal(true)
-    })
-
-    it('Should be able to change saleStatus to presale', async () => {
-      await expect(contract.connect(user1).mint(1)).to.be.revertedWith('Sale is not active')
-
-      await contract.setWhitelist([user1.address])
-      await contract.setSaleStatus(1)
-
-      await contract.connect(user1).mint(1)
-
-      const response = await contract.balanceOf(user1.address, 1)
-
-      expect(response).to.equal(1)
-    })
-
-    it('Should not be able for non-whitelisted addresses to mint', async () => {
-      await contract.setSaleStatus(1)
-      await expect(contract.connect(user1).mint(1)).to.be.revertedWith('You are not whitelisted')
-    })
-  })
-
-
+  })  
 })
